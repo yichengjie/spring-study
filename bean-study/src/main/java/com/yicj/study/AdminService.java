82543b17fe1bb256a1b4f5c7317dddf876d86c9b
易成杰 <626659321@qq.com>
2020-04-04 18:15:49 +0800

    保存

---
+++ bean-study/src/main/java/com/yicj/study/AdminService.java
@@ -0,0 +1,9 @@
+package com.yicj.study;
+
+import lombok.Data;
+
+@Data
+public class AdminService {
+    private String name;
+    private Integer age;
+}
--- bean-study/src/test/java/com/yicj/study/MyTestBeanTest.java
+++ bean-study/src/test/java/com/yicj/study/MyTestBeanTest.java
@@ -1,7 +1,11 @@
 package com.yicj.study;
 
 import org.junit.Test;
+import org.springframework.beans.factory.support.ChildBeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import static org.junit.Assert.* ;
 
@@ -13,6 +17,28 @@ public class MyTestBeanTest {
         XmlBeanFactory beanFactory = new XmlBeanFactory(resource);
         MyTestBean myTestBean = beanFactory.getBean("myTestBean", MyTestBean.class);
         assertEquals("testStr",myTestBean.getTestStr());
+    }
+
+
+    @Test
+    public void testBeanDefinition(){
+        GenericApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 自定义BeanDefinition,给其设置属性值，并且注册到Spring当中。
+        RootBeanDefinition rootBeanDefinition = new RootBeanDefinition();
+        rootBeanDefinition.getPropertyValues().add("name", "micheal");
+        rootBeanDefinition.setBeanClass(AdminService.class);
+
+        // 注意，这里必须要提供一个父BeanDefinition
+        ChildBeanDefinition childBeanDefinition = new ChildBeanDefinition("a1");
+        childBeanDefinition.getPropertyValues().add("age", 18);
+
+
+        applicationContext.registerBeanDefinition("a1", rootBeanDefinition);
+        applicationContext.registerBeanDefinition("a2", childBeanDefinition);
+        applicationContext.refresh();
+
+        System.out.println("a1:" + applicationContext.getBean("a1"));
+        System.out.println("a2:" + applicationContext.getBean("a2"));
     }
 
 }
