126572e2657f9a139b8cb9abd26ca7c3d4de6327
易成杰 <626659321@qq.com>
2020-04-14 10:36:58 +0800

    保存'

--- mybatis-spring-study/src/test/java/com/yicj/mapper/UserMapperTest.java
+++ mybatis-spring-study/src/test/java/com/yicj/mapper/UserMapperTest.java
@@ -3,13 +3,13 @@ package com.yicj.mapper;
 import com.yicj.model.User;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserMapperTest {
 
     @Test
     public void getUser(){
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserMapper userMapper = context.getBean("userMapper", UserMapper.class);
         User user = userMapper.getUser(1);
         System.out.println(ReflectionToStringBuilder.toString(user));
