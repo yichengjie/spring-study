619f7b6df0bb63c4075bca36a9f7bd23f3f40b0d
易成杰 <626659321@qq.com>
2020-04-14 14:54:41 +0800

    保存

--- mybatis-spring-study/src/main/java/com/yicj/mapper/UserMapper.java
+++ mybatis-spring-study/src/main/java/com/yicj/mapper/UserMapper.java
@@ -2,9 +2,10 @@ package com.yicj.mapper;
 
 import com.yicj.model.User;
 
-public interface UserMapper {
+import java.util.List;
 
+public interface UserMapper {
     void insertUser(User user) ;
     User getUser(Integer id) ;
-
+    List<User> selectAll() ;
 }
--- mybatis-spring-study/src/test/java/com/yicj/mapper/UserMapperTest.java
+++ mybatis-spring-study/src/test/java/com/yicj/mapper/UserMapperTest.java
@@ -1,19 +1,39 @@
 package com.yicj.mapper;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.yicj.model.User;
 import com.yicj.utils.RequestContextHolder;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.util.List;
+
 public class UserMapperTest {
+    private GenericXmlApplicationContext context ;
+    private UserMapper userMapper ;
+
+
+    @Before
+    public void before(){
+        RequestContextHolder.getInstance().setAirlineCode("XX");
+        context = new GenericXmlApplicationContext("applicationContext.xml");
+        userMapper = context.getBean("userMapper", UserMapper.class);
+    }
 
     @Test
     public void getUser(){
-        RequestContextHolder.getInstance().setAirlineCode("XX");
-        GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserMapper userMapper = context.getBean("userMapper", UserMapper.class);
         User user = userMapper.getUser(1);
         System.out.println(ReflectionToStringBuilder.toString(user));
+    }
+
+    @Test
+    public void selectAllUser(){
+        PageHelper.startPage(1,3);
+        List<User> users = userMapper.selectAll();
+        PageInfo pageInfo = new PageInfo(users) ;
+        System.out.println(pageInfo);
     }
 }
